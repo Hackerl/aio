43b75deda6fb1b05fd82d86ce69116c7a45b6391
Hackerl <490021209@qq.com>
2022-10-04 11:54:49 +0800

    Add#network stream listener

--- include/aio/net/stream.h
+++ include/aio/net/stream.h
@@ -3,8 +3,25 @@
 
 #include <aio/context.h>
 #include <aio/ev/buffer.h>
+#include <event2/listener.h>
 
 namespace aio::net {
+    class Listener : public std::enable_shared_from_this<Listener> {
+    public:
+        explicit Listener(const Context &context, evconnlistener *listener);
+        ~Listener();
+
+    public:
+        std::shared_ptr<zero::async::promise::Promise<std::shared_ptr<ev::IBuffer>>> accept();
+
+    private:
+        Context mContext;
+        evconnlistener *mListener;
+        std::shared_ptr<zero::async::promise::Promise<evutil_socket_t>> mPromise;
+    };
+
+    std::shared_ptr<Listener> listen(const Context &context, const std::string &host, short port);
+
     std::shared_ptr<zero::async::promise::Promise<std::shared_ptr<ev::IBuffer>>>
     connect(const Context &context, const std::string &host, short port);
 }
--- src/net/stream.cpp
+++ src/net/stream.cpp
@@ -1,6 +1,66 @@
 #include <aio/net/stream.h>
 #include <cstring>
 
+aio::net::Listener::Listener(const Context &context, evconnlistener *listener) : mContext(context), mListener(listener) {
+    struct stub {
+        static void onAccept(evconnlistener *listener, evutil_socket_t fd, sockaddr *addr, int socklen, void *arg) {
+            std::shared_ptr(static_cast<Listener *>(arg)->mPromise)->resolve(fd);
+        }
+
+        static void onError(evconnlistener *listener, void *arg) {
+            std::shared_ptr(static_cast<Listener *>(arg)->mPromise)->reject({-1, evutil_socket_error_to_string(EVUTIL_SOCKET_ERROR())});
+        }
+    };
+
+    evconnlistener_disable(mListener);
+    evconnlistener_set_cb(mListener, stub::onAccept, this);
+    evconnlistener_set_error_cb(mListener, stub::onError);
+}
+
+aio::net::Listener::~Listener() {
+    evconnlistener_free(mListener);
+}
+
+std::shared_ptr<zero::async::promise::Promise<std::shared_ptr<aio::ev::IBuffer>>> aio::net::Listener::accept() {
+    if (mPromise)
+        return zero::async::promise::reject<std::shared_ptr<aio::ev::IBuffer>>({-1, "pending request not completed"});
+
+    return zero::async::promise::chain<evutil_socket_t>([this](const auto &p) {
+        mPromise = p;
+        evconnlistener_enable(mListener);
+    })->then([this](evutil_socket_t fd) -> std::shared_ptr<ev::IBuffer> {
+        return std::make_shared<ev::Buffer>(bufferevent_socket_new(mContext.base, fd, BEV_OPT_CLOSE_ON_FREE));
+    })->finally([self = shared_from_this()]() {
+        evconnlistener_disable(self->mListener);
+        self->mPromise.reset();
+    });
+}
+
+std::shared_ptr<aio::net::Listener> aio::net::listen(const aio::Context &context, const std::string &host, short port) {
+    sockaddr_in sin = {};
+
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+
+    if (evutil_inet_pton(sin.sin_family, host.c_str(), &sin.sin_addr) < 0)
+        return nullptr;
+
+    evconnlistener *listener = evconnlistener_new_bind(
+            context.base,
+            nullptr,
+            nullptr,
+            LEV_OPT_CLOSE_ON_FREE | LEV_OPT_REUSEABLE,
+            -1,
+            (sockaddr *) &sin,
+            sizeof(sin)
+    );
+
+    if (!listener)
+        return nullptr;
+
+    return std::make_shared<Listener>(context, listener);
+}
+
 std::shared_ptr<zero::async::promise::Promise<std::shared_ptr<aio::ev::IBuffer>>>
 aio::net::connect(const aio::Context &context, const std::string &host, short port) {
     bufferevent *bev = bufferevent_socket_new(context.base, -1, BEV_OPT_CLOSE_ON_FREE);
